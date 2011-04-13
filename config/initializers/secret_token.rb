25ab1263b7426a4a76c53396d927b3664ee15966
Dennis Oelkers <dennis@lauschmusik.de>
2011-04-13 14:40:45 +0200

    - initial project import - added sorcery to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delivery::Application.config.secret_token = 'd4b47edd9e9a70171b3111080e49bd92a242c23a57fd07ebcd752cfdfe8ca35ce9e8bd986f64bfc3fe1ca3f6ba5b6df714f3f78ae537cbcd97893913f9f985ed'
